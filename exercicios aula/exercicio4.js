49a00c13f913deeb512b97764aeaa65e63581775
carlosneves11 <carlosneves11@gmail.com>
2022-07-18 22:59:27 -0300

    exercicio6

--- exercicios aula/exercicio4.js
+++ exercicios aula/exercicio4.js
@@ -1,9 +1,11 @@
 const entrada = require ('readline-sync');
 
 const quantmaca = entrada.questionInt('\nInforme a quantidade de macas  ');
+let valtot = 0;
 if (quantmaca < 12) {
-    console.log(' o valor total é: ' + parseFloat(quantmaca * 0.30));
+    valtot = quantmaca * 0.30;
 } else {
-    console.log(' o valor total é: ' + parseFloat(quantmaca* 0.25));
-    
+    valtot = quantmaca * 0.25;   
 }
+
+console.log(' o valor total é: ' + valtot);
